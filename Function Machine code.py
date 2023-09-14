6ffae7f93030d8177ae1c8171f2480d250d18ae9
DiegoA00573953 <145053285+DiegoA00573953@users.noreply.github.com>
2023-09-14 12:44:30 -0600

    Create Function Machine code.py
    Some exercises about how functions work

---
+++ Function Machine code.py
@@ -0,0 +1,30 @@
+#Some exercises about how to do procces
+def f(L): 
+  f_l = L
+  return f_l
+q= f(10)
+print(q)
+
+def f(x):
+  f_x=-2*x+34
+  return f_x
+y=f(10)
+print(y)
+
+def f(x):
+  f_x = (-x /3) - 10
+  return f_x
+y=f(9)
+print(y)
+
+def f(x):
+  f_x= -abs(3*x)
+  return f_x
+y=f(3)
+print(y)
+
+def f(x):
+  f_x = (x-2)**2
+  return f_x
+y=f(4)
+print(y)
