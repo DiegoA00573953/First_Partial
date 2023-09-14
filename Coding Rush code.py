e71974d5484922e607dc8219aa6de4f4e4ac9d01
DiegoA00573953 <145053285+DiegoA00573953@users.noreply.github.com>
2023-09-14 12:47:43 -0600

    Create Coding Rush code.py
    The challenge was to change the order of the letters so that stressed become dessert

---
+++ Coding Rush code.py
@@ -0,0 +1,9 @@
+#inversion word for desserts
+def dessert():
+  print("Give me a word, please pal")
+  word = input()
+  print("give me a second pls")
+  inverted_word = word[::-1]
+  print(inverted_word)
+  print("I hope you liked it :)")
+dessert()
